d1c8c24009c0e9e111d66296571b8a813854b2d7
98manudi <68694057+98manudi@users.noreply.github.com>
2021-05-20 09:08:55 +0530

    manudi added home page

--- src/itpm_projectnb/Home/Controller.java
+++ src/itpm_projectnb/Home/Controller.java
@@ -5,9 +5,16 @@
  */
 package itpm_projectnb.Home;
 
-import com.jfoenix.controls.JFXTimePicker;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,64 +26,125 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-/**
- * FXML Controller class
- *
- * @author Sudarshana
- */
-public class Controller implements Initializable {
+public class Controller implements Initializable{
 
     @FXML
     private Button btnLecturer;
     @FXML
-    private Button btnSubjects;
-    @FXML
-    private Button btnDashBoard;
-    @FXML
     private Button btnSession;
-    
-    
-
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-
     @FXML
-    private void handleButtonAction(ActionEvent event) {
+    private Button btnSubjects;
+  
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+    public void handleButtonAction(ActionEvent event){
         if(event.getSource() == btnLecturer){
-            
-             loadStage("lecturer.fxml");
-        }
-        else if(event.getSource() == btnSubjects){
+            loadStage("lecturer.fxml");
+        
+        }else if(event.getSource() == btnSubjects){
             loadStage("subjects.fxml");
-        }else if(event.getSource() == btnDashBoard){
-            loadStage("dashboard.fxml");
         }else if(event.getSource() == btnSession){
-            loadStage("Location.fxml");
+            loadStage("sessions.fxml");
         }
     }
-    public void loadStage(String fxml){
+    
+    public void loadStage(String fxml) {
         try {
             Parent root = FXMLLoader.load(getClass().getResource(fxml));
+
             Stage stage = new Stage();
             stage.close();
             stage.setScene(new Scene(root));
             stage.show();
-            
-            
+
         } catch (IOException ex) {
             Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
+
+    @FXML
+    private void handlebtnwork(ActionEvent event) throws IOException {
+           Parent root = FXMLLoader.load(getClass().getResource("Workshedule.fxml"));//new fxml for a new window
+           
+        Stage stage = new Stage();//object for a new window
+        stage.setTitle("Working Days and Hours");
+        stage.setScene(new Scene(root, 1400, 662));
+        //specifies modality for a new window
+        
+        //application_modal means you cannot interact with either window until this new window is closed.
+       stage.initModality(Modality.APPLICATION_MODAL);//default is none
+//       stage.initOwner(btnwshedule.getScene().getWindow());
+//        stage.resizableProperty().setValue(Boolean.FALSE);
+         stage.initStyle(StageStyle.UTILITY);//hides minimize and maximize
+        stage.show();
+        
+    }
     
+    @FXML
+    private void handlebtnmanagesettings(ActionEvent event) throws IOException {
+           Parent root = FXMLLoader.load(getClass().getResource("managesetting_page1.fxml"));//new fxml for a new window
+           
+        Stage stage = new Stage();//object for a new window
+        stage.setTitle("Manage Settings");
+        stage.setScene(new Scene(root, 700, 600));
+        //specifies modality for a new window
+        
+        //application_modal means you cannot interact with either window until this new window is closed.
+       stage.initModality(Modality.APPLICATION_MODAL);//default is none
+//       stage.initOwner(btnwshedule.getScene().getWindow());
+//        stage.resizableProperty().setValue(Boolean.FALSE);
+         stage.initStyle(StageStyle.UTILITY);//hides minimize and maximize
+        stage.show();
+        
+    }
+    
+     @FXML
+    private void handlebtnTimetable(ActionEvent event) throws IOException {
+           Parent root = FXMLLoader.load(getClass().getResource("Timetable.fxml"));//new fxml for a new window
+           
+        Stage stage = new Stage();//object for a new window
+        stage.setTitle("TimeTable");
+        stage.setScene(new Scene(root, 1087, 701));
+        //specifies modality for a new window
+        
+        //application_modal means you cannot interact with either window until this new window is closed.
+       stage.initModality(Modality.APPLICATION_MODAL);//default is none
+//       stage.initOwner(btnwshedule.getScene().getWindow());
+//        stage.resizableProperty().setValue(Boolean.FALSE);
+         stage.initStyle(StageStyle.UTILITY);//hides minimize and maximize
+        stage.show();
+         
+    }
+    
+  
+    @FXML
+    public void loadAddStudentGroups(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource("addStudentGroups.fxml"));
+       Scene scene = new Scene(pane);
+
+       Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+       window.setScene(scene);
+       window.centerOnScreen();
+    }
+    
+    @FXML
+    public void loadTags(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource("addTags.fxml"));
+       Scene scene = new Scene(pane);
+
+       Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+       window.setScene(scene);
+       window.centerOnScreen();
+    }   
 }
+
